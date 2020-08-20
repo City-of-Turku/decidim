96dcc90a8c6146aeac7b0d83c530788cb3272cce
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-08-20 20:01:47 +0300

    Collect available metadata from all authorization methods

--- lib/turku/tunnistamo_metadata_collector.rb
+++ lib/turku/tunnistamo_metadata_collector.rb
@@ -5,17 +5,29 @@ module Turku
     def metadata
       super.tap do |data|
         if authentication_method == "turku_suomifi"
+          # Suomi.fi
           data.merge!(
             birthdate: raw_info[:birthdate],
             municipality_code: raw_info.dig(:address, :municipality_code),
             municipality_name: raw_info.dig(:address, :municipality_name),
             postal_code: raw_info.dig(:address, :postal_code)
           )
-        elsif authentication_method == "turku_opasid"
-          # TODO: The OpasID is so far untested and not known what kind of data
-          #       it contains
+        elsif authentication_method == "opas_adfs"
+          # Students ADFS
+          data.merge!(role: student_role)
+        elsif authentication_method == "axiell_aurora"
+          # Library card
+          data.merge!(birthdate: raw_info[:birthdate])
         end
       end
+    end
+
+    private
+
+    def student_role
+      return "student" if raw_info[:is_student]
+
+      nil
     end
   end
 end
