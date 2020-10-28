a8e119d07de4c5f67bb3dcdcf9195cb165098636
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-10-28 17:28:27 +0200

    Implement sv logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def logo_image_tag(version: nil)
+    suffix = current_locale == "sv" ? "-sv" : ""
+    suffix += "-#{version}" if version
+
+    image_location = "turku/logo#{suffix}.svg"
+    image_tag(
+      image_location,
+      alt: t("logo", scope: "decidim.accessibility", organization: current_organization.name)
+    )
+  end
 end
