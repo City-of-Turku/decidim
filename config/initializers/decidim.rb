023d6eea617986a7c663932971303c9ba7129332
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-06-09 14:30:53 +0300

    Apply the mailer sender and change application name

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Decidim.configure do |config|
-  config.application_name = "My Application Name"
-  config.mailer_sender = "change-me@domain.org"
+  config.application_name = "Turku Asukasbudjetti"
+  config.mailer_sender = Rails.application.config.mailer_sender
 
   # Change these lines to set your preferred locales
   config.default_locale = :fi
