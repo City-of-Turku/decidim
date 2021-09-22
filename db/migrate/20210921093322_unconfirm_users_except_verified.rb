acb83620696b8d6778a74846afa3cb9dc56c3ff6
Eero Lahdenperä <eero.lahdenpera@mainiotech.fi>
2021-09-22 13:11:37 +0300

    Add unconfirm unverified users migration

---
+++ db/migrate/20210921093322_unconfirm_users_except_verified.rb
@@ -0,0 +1,9 @@
+class UnconfirmUsersExceptVerified < ActiveRecord::Migration[5.2]
+  def up
+    Decidim::Authorization.find_each do |authorization|
+      next if authorization.metadata.is_a?(Hash) && %w(opas_adfs turku_adfs).include?(authorization.metadata["service"])
+
+      authorization.user.update(confirmed_at: nil)
+    end
+  end
+end
