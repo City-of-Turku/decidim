27c86f6ece3871d6a50adfa37ab5e0dd40b50818
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-09-28 13:48:32 +0300

    Unconfirm only Tunnistamo authorized users

--- db/migrate/20210921093322_unconfirm_users_except_verified.rb
+++ db/migrate/20210921093322_unconfirm_users_except_verified.rb
@@ -1,6 +1,6 @@
 class UnconfirmUsersExceptVerified < ActiveRecord::Migration[5.2]
   def up
-    Decidim::Authorization.find_each do |authorization|
+    Decidim::Authorization.where(name: "tunnistamo_idp").find_each do |authorization|
       next if authorization.metadata.is_a?(Hash) && %w(opas_adfs turku_adfs).include?(authorization.metadata["service"])
 
       authorization.user.update(confirmed_at: nil)
