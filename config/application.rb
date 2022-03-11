f8528977210c0cdcd318d28253b2c6e2dacc208c
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-03-11 16:57:03 +0200

    Add the action cable engine necessary for non-development operation

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,8 @@
 require_relative "boot"
 
 require "decidim/rails"
+# Add the frameworks used by your app that are not loaded by Decidim.
+require "action_cable/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
