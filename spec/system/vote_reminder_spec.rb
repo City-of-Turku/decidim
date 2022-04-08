5549b5214a1a89becf44b501745a6f5b73c4cfd7
Eero Lahdenperä <eero.lahdenpera@mainiotech.fi>
2022-04-08 16:50:30 +0300

    Fix reminder email links

--- spec/system/vote_reminder_spec.rb
+++ spec/system/vote_reminder_spec.rb
@@ -4,12 +4,14 @@ require "rails_helper"
 
 describe "Admin sends vote reminders", type: :system do
   include_context "when managing a component as an admin"
-  let(:organization) { create(:organization, available_locales: [:en, :fi]) }
+  let(:organization) { create(:organization, available_locales: [:en, :fi], host: host) }
   let(:user) { create(:user, :admin, :confirmed, name: "Sarah Kerrigan", organization: organization, locale: "en") }
   let(:user2) { create(:user, :confirmed, name: "Jim Raynor", organization: organization, locale: "en") }
   let(:component) { create(:component, manifest_name: "budgets", organization: organization) }
   let(:budget) { create(:budget, component: component, total_budget: 26_000_000) }
   let(:projects) { create_list(:project, 1, budget: budget, budget_amount: 25_000_000) }
+  let(:base_url) { "http://#{host}" }
+  let(:host) { "whatever.lvh.me" }
 
   let!(:order) do
     order = create(:order, user: user, budget: budget)
@@ -57,8 +59,8 @@ describe "Admin sends vote reminders", type: :system do
       emails.each do |email|
         expect(email.subject).to eq("You have an unfinished vote in the participatory budgeting vote")
       end
-      expect(last_email_first_link).to eq("/processes/#{component.participatory_space.slug}/f/#{component.id}/budgets/#{budget.id}")
-      expect(last_email_link).to eq("/processes/#{component.participatory_space.slug}/f/#{component.id}/")
+      expect(last_email_first_link).to eq("#{base_url}/processes/#{component.participatory_space.slug}/f/#{component.id}/budgets/#{budget.id}")
+      expect(last_email_link).to eq("#{base_url}/processes/#{component.participatory_space.slug}/f/#{component.id}/")
     end
 
     it "doesnt remind twice" do
