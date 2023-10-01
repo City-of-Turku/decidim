6e3dff22f5a2a2bb9352109f0533ee9db60676d9
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-10-01 13:30:09 +0300

    Remove the vote reminder customizations as they are no longer needed
    As this feature was introduced in Decidim 0.27.

--- config/application.rb
+++ config/application.rb
@@ -86,7 +86,6 @@ module DecidimTurku
         )
 
         # Model extensions
-        ::Decidim::User.include(UserExtensions)
         ::Decidim::ActionLog.include(ActionLogExtensions)
 
         # Controller extensions
