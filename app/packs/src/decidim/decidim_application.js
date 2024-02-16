298858916791c180cc88f7ce8d5437bf018e46bd
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2024-02-16 14:41:32 +0200

    Remove the custom exit handler
    This is no longer needed as this is already part of the
    decidim-budgets module.

--- app/packs/src/decidim/decidim_application.js
+++ app/packs/src/decidim/decidim_application.js
@@ -1,20 +1,5 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-// = require jquery
-// = require rails-ujs
-// = require activestorage
-// = require decidim
-// = require turku/exit_handler
-// = require_self
+// This file is compiled inside Decidim core pack. Code can be added here and
+// will be executed as part of that pack.
 
-import "src/turku/exit_handler"
+// Load images
+require.context("../../images", true)
