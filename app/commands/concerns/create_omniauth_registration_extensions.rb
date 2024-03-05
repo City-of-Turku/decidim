9847696235e5aebe47b428706ca1cbdc9f23dd84
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2024-03-05 11:57:14 +0200

    Fix create omniauth registration with the privacy module

--- app/commands/concerns/create_omniauth_registration_extensions.rb
+++ app/commands/concerns/create_omniauth_registration_extensions.rb
@@ -4,6 +4,19 @@ module CreateOmniauthRegistrationExtensions
   extend ActiveSupport::Concern
 
   included do
+    def existing_identity
+      @existing_identity ||= begin
+        identity = ::Decidim::Identity.find_by(
+          user: organization.users.entire_collection,
+          provider: form.provider,
+          uid: form.uid
+        )
+
+        identity.update(turku_oid: anonymized_oid) if identity && !identity.turku_oid && anonymized_oid
+        identity
+      end
+    end
+
     private
 
     alias_method :create_identity_orig_turku, :create_identity unless private_method_defined?(:create_identity_orig_turku)
@@ -12,19 +25,6 @@ module CreateOmniauthRegistrationExtensions
       identity = create_identity_orig_turku
       identity.update(turku_oid: anonymized_oid) if !identity.turku_oid && anonymized_oid
       identity
-    end
-
-    def existing_identity
-      @existing_identity ||= begin
-        identity = ::Decidim::Identity.find_by(
-          user: organization.users,
-          provider: form.provider,
-          uid: form.uid
-        )
-
-        identity.update(turku_oid: anonymized_oid) if identity && !identity.turku_oid && anonymized_oid
-        identity
-      end
     end
 
     def anonymized_oid
